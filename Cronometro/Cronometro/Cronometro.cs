85e971fb004ecffb23d8d96b13b448e153d5d5e9
simorai <109289730+simorai@users.noreply.github.com>
2024-12-20 17:23:54 +0000

    Teste

---
+++ Cronometro/Cronometro/Cronometro.cs
@@ -0,0 +1,48 @@
+﻿namespace Cronometro
+{
+    public class Cronometro
+    {
+        #region Atributos
+
+        private DateTime _start; //inicio do cronometro
+        private DateTime _stop; //fim do cronometro
+        private bool _isrunning; //indica se esta ligado ou desligado
+
+        #endregion
+
+        public void StartClock() //metodo que liga o cronometro
+        {
+            if (_isrunning) // se já não estiver ligado...
+                throw new InvalidOperationException("O cronometro já esta ligado");
+
+            _start = DateTime.Now; // vai buscar a data do sistema na altura
+            _isrunning = true; // cronometro ligado
+        }
+
+        public void StopClock() //metodo que desliga o cronometro
+        {
+            if (!_isrunning) // senao estiver desligado...
+                throw new InvalidOperationException("O cronometro já esta desligado");
+
+            _stop = DateTime.Now; // vai buscar a data do sistema e guarda no _stop
+            _isrunning = false; //desliga o cronometro
+        }
+
+        public TimeSpan GetTimeSpan() // TimeSpan representa um intervalo de tempo ou duração
+        {
+            return _stop - _start; // obtem o valor entre o começo e o fim do relogio
+        }
+
+        public bool ClockState() //metodo que retorna o estado do cronometro, ligado/desligado
+        {
+            return _isrunning;
+        }
+
+        public DateTime StarTime() // retorna o valor de inicio
+        {
+            return _start;
+        }
+
+
+    }
+}
---
+++ Cronometro/Cronometro/Form1.cs
@@ -0,0 +1,10 @@
+namespace Cronometro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
